dbd6beea06649bdd81784bf90c93bcbdef895849
Phuc Nguyen <hoangphuc1903@gmail.com>
2025-05-09 10:57:01 +0700

    feat: Add nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,2 @@
+require("config.vimrc")
+require("config.lazy")
---
+++ nvim/lua/config/vimrc.lua
@@ -0,0 +1,13 @@
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set cursorline")
+
+-- Switch focus buffer keys
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
